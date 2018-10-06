0d446bd635b6d9d04ebec276caa4e518d234b2bd
soleh12 <43886958+soleh12@users.noreply.github.com>
2018-10-06 08:57:27 +0000

    Add files via upload

---
+++ balikkata.js
@@ -0,0 +1,9 @@
+function soleh(str){
+	return str.split('').reverse().join('');
+}
+console.log(soleh('jakarta'));
+console.log(soleh('Hello World and Coders')); // sredoC dna dlroW olleH
+console.log(soleh('John Doe')); // eoD nhoJ
+console.log(soleh('I am a bookworm')); // mrowkoob a ma I
+console.log(soleh('Coding is my hobby')); // ybboh ym si gnidoC
+console.log(soleh('Super')); // repuS
